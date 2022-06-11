ff65914730f07cbe487abe5fe4738a416216b56c
Ibrahim Ibrahim <ii83326@gmail.com>
2022-06-11 15:24:04 +0100

    feat: Connected to mongo database

---
+++ mongo.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+
+
+if(process.argv.length < 5){
+  if(process.argv.length != 3) {
+    console.log('Please provide password, name and number to add to the db')
+    process.exit(1)
+  }
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.rm5rhro.mongodb.net/personsApp?retryWrites=true&w=majority`
+
+personSchema = mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const connection = mongoose.connect(url)
+
+if (process.argv.length == 3) {
+    connection.then((result) => {
+      console.log('connected!!')
+
+      Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(person)
+        })
+        mongoose.connection.close()
+      })
+    })
+    .catch(err => console.log(err))
+
+} else {
+    connection.then((result) => {
+  
+      console.log('connected')
+  
+      const person = new Person({
+        name: process.argv[3], 
+        number: process.argv[4]
+      })
+  
+      return person.save()
+    })
+    .then(() => {
+      console.log('saved person!')
+      return mongoose.connection.close()
+    })
+    .catch((err) => console.log(err))
+}
+
