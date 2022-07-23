28389d7ecc68fb065754b98ee9b037879e145cdc
Ibrahim Ibrahim <ii83326@gmail.com>
2022-07-23 20:33:47 +0100

    feat: added toJSON function to model

--- index.js
+++ index.js
@@ -24,16 +24,14 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if(!person) {
-    response.status(404).json({
-      error: 'invalid id'
-    }).end()
-  }
-
-  response.json(person)
+  Person.findById(request.params.id).then(person => {
+    if(!person) {
+      response.status(404).json({
+        error: 'invalid id'
+      }).end()
+    }
+    response.json(person)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
--- models/persons.js
+++ models/persons.js
@@ -15,4 +15,12 @@ personSchema = mongoose.Schema({
   number: String
 })
 
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 module.exports = mongoose.model('Person', personSchema)
